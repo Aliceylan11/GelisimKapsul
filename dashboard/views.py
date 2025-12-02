1bf9960e2527ababd960f2f4c7f08513539bb2bc
Sperrex <ardairmak5555@gmail.com>
2025-12-02 23:15:29 +0300

    arda'nin ozel calismalari

--- dashboard/views.py
+++ dashboard/views.py
@@ -1,3 +1,37 @@
 from django.shortcuts import render
 
-# Create your views here.
+
+def home(request):
+    context_dersler = [
+        {
+            "id": 1,
+            "baslik": "Python ve Django",
+            "aciklama": "Gelişim Kapsül projesi için backend geliştirme temelleri.",
+            "resim": "https://placehold.co/600x400/1A374D/white?text=Django", 
+            "egitmen": "Arda IRMAK"
+        },
+        {
+            "id": 2,
+            "baslik": "Web Tasarım (UI/UX)",
+            "aciklama": "Bootstrap ve CSS ile modern, responsive arayüzler.",
+            "resim": "https://placehold.co/600x400/F2994A/white?text=Tasarim",
+            "egitmen": "Edanur BOZLAR"
+        },
+        {
+            "id": 3,
+            "baslik": "Veri Tabanı Yönetimi",
+            "aciklama": "SQLite veritabanı modellemesi ve sorgular.",
+            "resim": "https://placehold.co/600x400/406882/white?text=SQL",
+            "egitmen": "Hasan Hüseyin KESKİN"
+        },
+        {
+            "id": 4,
+            "baslik": "Mobil Programlama",
+            "aciklama": "Mobil uyumlu yapılar ve uygulama geliştirme.",
+            "resim": "https://placehold.co/600x400/2D3436/white?text=Mobil",
+            "egitmen": "Ali CEYLAN"
+        },
+        
+    ]
+
+    return render(request, 'home.html', {"dersler": context_dersler})
