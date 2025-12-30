afeb6607526e838d34ce0af02f3847a31b174dcf
Sperrex <ardairmak5555@gmail.com>
2025-12-30 10:40:04 +0300

    Home template güncellendi: Yeni kart stili, slice filtresi ve materyal listesi linkleri eklendi.

--- dashboard/urls.py
+++ dashboard/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path, include
 from . import views 
 
+
 urlpatterns = [
     path('', views.home, name='dashboard/home'),
     path('gizlilik-politikasi/', views.privacy_policy, name='privacy'),
